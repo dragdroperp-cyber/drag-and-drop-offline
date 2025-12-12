2727457e0e15caa81e4e954374c890b8410c4c0f
dragdroperp-cyber <dragdroperp@gmail.com>
2025-12-12 18:32:42 +0530

    Update version.js

--- src/utils/version.js
+++ src/utils/version.js
@@ -1,4 +1,5 @@
 // App version information
 // Keep this in sync with CACHE_VERSION in service-worker.js
-export const APP_VERSION = 'v1.0.0';
+export const APP_VERSION = 'v1.0.1';
 export const APP_NAME = 'Grocery ERP';
+
